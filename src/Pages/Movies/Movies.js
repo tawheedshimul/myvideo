3dc8e20796c1add43d8842d120bbc2733e50f8cc
MD TAWHEED SHIMUL <tawheedshimul@gmail.com>
2023-06-22 18:55:59 +0600

    update

--- src/Pages/Movies/Movies.js
+++ src/Pages/Movies/Movies.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import MovieSearch from './MovieSearch';
+import MovieList from './MovieList';
 
 const Movies = () => {
     return (
         <div>
             <MovieSearch></MovieSearch>
+            <MovieList></MovieList>
         </div>
     );
 };
